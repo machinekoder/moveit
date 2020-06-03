1267e779d34627e6c1635a7f22431615486e98f3
Henning Kayser <henningkayser@picknik.ai>
2020-06-03 19:50:34 +0200

    Cleanup includes in moveit_cpp

--- moveit_ros/planning/moveit_cpp/src/moveit_cpp.cpp
+++ moveit_ros/planning/moveit_cpp/src/moveit_cpp.cpp
@@ -36,13 +36,6 @@
 
 #include <stdexcept>
 #include <moveit/moveit_cpp/moveit_cpp.h>
-#include <moveit/planning_scene_monitor/current_state_monitor.h>
-#include <moveit/common_planning_interface_objects/common_objects.h>
-
-#include <tf2/utils.h>
-#include <tf2_ros/transform_listener.h>
-#include <ros/console.h>
-#include <ros/ros.h>
 
 namespace moveit_cpp
 {
